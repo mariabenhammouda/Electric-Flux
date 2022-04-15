5def60a279fd930a8570191db9be1f210a7df88a
mariabenhammouda <maria_anfal@hotmail.com>
2022-04-15 08:26:50 +0400

    changed rate of fade of spheres

--- cyclicalanimation/src/ofApp.cpp
+++ cyclicalanimation/src/ofApp.cpp
@@ -19,19 +19,20 @@ void ofApp::draw(){
     int time=ofGetElapsedTimeMillis()/10;
     cam.begin();
     ofPushMatrix();
-    ofSetColor(20, 75, 190,ofGetFrameNum() % 280);
+    
     ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
     for ( int i=0;i<500;i+=25){
         for(int n=1; n<350; n+=5){
+            ofSetColor(20, 75, 190,ofGetFrameNum() % 300);
             ofDrawSphere(i, 0, (n*10), 10);
         ofRotateDeg(time % (10+n));
-            ofPushStyle();
+           
             ofSetColor(255, 0, 0);
             ofSetLineWidth(3);
             ofDrawLine(0, 0, (350*10), 0, 0, 0);
             ofSetColor(255, 255, 255);
             ofDrawBox(0, 0, (n*10), 30, 30, 30);
-            ofPopStyle();
+           
         }
     }
     ofPopMatrix();
